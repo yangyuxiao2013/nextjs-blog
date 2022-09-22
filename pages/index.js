64c209bf07b2e3352940a991e37587ff922e028f
yangyuxiao2013 <yangyuxiao2013@gmial.com>
2022-09-22 21:22:44 +0900

    001

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,23 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog/hello-world">
+              <a>Blog Post</a>
+            </Link>
+          </li>
+        </ul>
       </section>
     </Layout>
   );
